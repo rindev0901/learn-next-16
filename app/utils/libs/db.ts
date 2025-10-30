b432ec5b6deb186d0e9e394f20f0316e5c33ecef
Nguyen Huynh Duc <duc2.nh@vdsc.com.vn>
2025-10-30 12:55:00 +0700

    fix(db): use DB_POSTGRES_URL environment variable for pg Pool connection

--- app/utils/libs/db.ts
+++ app/utils/libs/db.ts
@@ -1,13 +1,13 @@
 import { Pool } from "pg";
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DB_POSTGRES_URL) {
 	throw new Error(
 		"DATABASE_URL environment variable is not defined. Please add it to your .env.local file."
 	);
 }
 
 const db = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DB_POSTGRES_URL,
 });
 
 export { db };
